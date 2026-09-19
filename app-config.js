9a3307aceaad18cade336873e74b7bdcc875ae2d
池边影 <iamchenboyi@gmail.com>
2026-09-19 14:51:20 +0800

    Bump compact home shell version

--- app-config.js
+++ app-config.js
@@ -12,7 +12,7 @@ try{
   if (app) app.style.visibility = "hidden";
   if (!document.querySelector('script[data-ui-v3]')) {
     const s = document.createElement("script");
-    s.src = "ui-v3.js?v=63";
+    s.src = "ui-v3.js?v=64";
     s.async = false;
     s.dataset.uiV3 = "1";
     document.head.appendChild(s);
